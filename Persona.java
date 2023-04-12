eae5eb5f0339d60bfae000a5c1435f2269659a57
NicoPS <119110500+xcovan@users.noreply.github.com>
2023-04-12 12:43:51 +0200

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,57 @@
+package javadoc1;
+
+/**
+ * Esta clase representa un objeto Coche.
+ *
+ * @author Alberto Ruiz
+ * @version 1.0
+ * @since 2022-03-01
+ */
+
+public class Persona {
+    
+    private String nombre;
+    private int edad;
+    
+    /**
+     * Constructor de la clase Persona.
+     * @param nombre El nombre de la persona.
+     * @param edad La edad de la persona.
+     */
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    
+    /**
+     * Devuelve el nombre de la persona.
+     * @return El nombre de la persona.
+     */
+    public String getNombre() {
+        return nombre;
+    }
+    
+    /**
+     * Establece el nombre de la persona.
+     * @param nombre El nombre de la persona.
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    /**
+     * Devuelve la edad de la persona.
+     * @return La edad de la persona.
+     */
+    public int getEdad() {
+        return edad;
+    }
+    
+    /**
+     * Establece la edad de la persona.
+     * @param edad La edad de la persona.
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
